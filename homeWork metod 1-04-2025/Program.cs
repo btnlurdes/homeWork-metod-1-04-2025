121b635c1cd559349efaed9829b7d3fb1ffcd68f
Bogdanova <Bogdanova@BOGDANOVAT>
2025-04-06 09:00:40 +0300

    Добавьте файлы проекта.

---
+++ homeWork metod 1-04-2025/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Поиск индекса первого совпадения. 
+Дан массив строк и строка образец. Нужно выдать индекс первого совпадения строки с образцом. Если нет совпадений вернуть -1.*/
+
+
+
+/*Развернуть массив строк в обратном порядке*/
+
+using System.Diagnostics.Tracing;
+
+class Program
+{
+    static string[] ReverseArray(string[] words),string outWords)
+        {
+            string[] words = new string[words.Length];
+            for (int i =words.Length-1; i>=0;i--)
+            {
+                outWords+= words[i];
+            }
+        return outWords;
+        }
+}
+
+static void Main(string[] args)
+    {
+        string[] input = { "тыква", "яблоко", "баклажан", "ранетка", "томаты" };
+        string[] output = ReverseArray;
+        Console.WriteLine("Результат в обратном порядке:"{result.outWords}");
+    }
